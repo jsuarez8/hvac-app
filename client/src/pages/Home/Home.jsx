aae4860561cb0c500323713be07316b03f10a023
Jose Suarez <jasuarez4261@eagle.fgcu.edu>
2023-11-08 20:07:42 -0500

    changed homepage layout and content

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import HomePageImage from "./../../img/man-relaxing-on-couch.png";
 import { Card, Flex, Typography } from "antd";
 import "./Home.scss";
 import ServiceBoxes from "../../components/ServiceBoxes/ServiceBoxes.jsx";
-
+import HomePageConsultation from "./../../img/fort-myers-city.png";
 const Home = () => {
   return (
     <div className="home">
@@ -24,30 +24,58 @@ const Home = () => {
               <Typography.Title level={3} className="slogan-text">
                 <p>
                   {" "}
-                  “Expert Plumbing + HVAC Service, Repairs and
-                  Replacement–Guaranteed.”
+                  “Expert Plumbing + HVAC Service, Repairs and Replacement -
+                  Guaranteed.”
                 </p>
               </Typography.Title>
             </Flex>
           </Flex>
         </Card>
       </div>
-
-      {/* Schedule Consultation Section */}
-      <div className="consultation-section">
-        <h2 className="consultation-title">Schedule a Free Consultation Now</h2>
-        <p>How can we help?</p>
-        {/* Dropdown with Services */}
-        <select name="services" id="services-dropdown">
-          <option value="none"></option>
-          <option value="service1">Filter Change</option>
-          <option value="service2">Service 2</option>
-          <option value="service3">Service 3</option>
-          {/* ... */}
-        </select>
-      </div>
       {/* Service Boxes Section */}
       <ServiceBoxes />
+
+      <div className="home-paragraph">
+        <div className="home-paragraph-container">
+          <div className="home-paragraph-title">
+            <h1>We're Excited To Meet You!</h1>
+            <p>
+              Welcome to Breeze Boss, the leading authority in HVAC excellence
+              serving the vibrant communities of Fort Myers, Naples, Estero, and
+              the broader Southwest Florida area. Our seasoned team of certified
+              professionals is dedicated to enhancing your indoor climate with
+              superior HVAC solutions. At Breeze Boss, we pride ourselves on our
+              legacy of quality, delivering state-of-the-art installations,
+              meticulous repairs, and all-encompassing maintenance services
+              designed for both residential and commercial settings. In the
+              unique warmth of Southwest Florida, Breeze Boss stands out by
+              providing customized services that focus on optimizing energy
+              efficiency and minimizing costs. We don't just address your HVAC
+              needs; we innovate and adapt to ensure that your living and
+              working spaces are epitomes of comfort and efficiency year-round.
+              Our commitment to your satisfaction is unwavering, as we blend
+              traditional values of service with cutting-edge technology.
+              Embrace the Breeze Boss difference and feel confident that your
+              indoor air quality and comfort are in expert hands. Reach out to
+              us for a personalized consultation, and take the first step
+              towards a cooler, more comfortable tomorrow.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="homepage-consultation">
+        <div className="homepage-consultation-container">
+          <img
+            src={HomePageConsultation}
+            alt="Fort Myers City"
+            className="homepage-consultation fort-myers-picture"
+          />
+          <h1 className="homepage-consultation-title">
+            We Proudly Serve All These Florida Communities
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
